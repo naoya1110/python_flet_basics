0142138c999c9f354192efe9249d5c947bf3d4a2
naoya1110 <naoya.1110@gmail.com>
2023-02-21 17:42:58 +0900

    updated

---
+++ 06_open_image.py
@@ -0,0 +1,68 @@
+# https://flet.dev/docs/controls/image
+
+import flet as ft
+import cv2
+import base64
+import numpy as np
+
+
+def main(page: ft.Page):
+    
+    # page(アプリ画面)の設定
+    page.title = "OPEN A IMAGE"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.bgcolor = ft.colors.INDIGO_50
+    page.padding = 50
+    page.window_height = 500
+    page.window_width = 800
+    
+    
+    # numpy画像データをbase64にエンコードする関数
+    def get_base64_img(img):
+        _, encoded = cv2.imencode(".jpg", img)
+        img_base64 = base64.b64encode(encoded).decode("ascii")
+        return img_base64
+    
+    # ファイルが選択された時のコールバック
+    def on_img_open(e: ft.FilePickerResultEvent):
+        filepath = e.files[0].path
+
+        # OpenCVで画像を読み込み
+        img = cv2.imread(filepath)
+        img = cv2.resize(img, dsize=None, fx=0.3, fy=0.3)
+        img_h, img_w, _ = img.shape
+        
+        # image_displayのプロパティを更新
+        image_display.src_base64 = get_base64_img(img)
+        image_display.height = img_h
+        image_display.width = img_w
+        
+        # pageをアップデート
+        page.update()
+    
+    
+    filepick_button = ft.ElevatedButton("Open Image", on_click=lambda _: file_picker.pick_files(allow_multiple=True))
+    
+    # 初期画像（ダミー）
+    img_blank = 255*np.ones((300, 300, 3), dtype="uint8")
+    img_h, img_w, _ = img_blank.shape
+    image_display = ft.Image(src_base64=get_base64_img(img_blank),
+                             width=img_w, height=img_h)
+    
+    page.add(filepick_button)
+    page.add(image_display)
+    
+    file_picker = ft.FilePicker(on_result=on_img_open)
+    page.overlay.append(file_picker)
+    page.update()
+    
+
+
+
+# デスクトップアプリとして開く
+ft.app(target=main)
+
+# webアプリとして開く場合は任意のポート番号を指定し
+# ブラウザでlocalhost:7777を開く
+# ft.app(target=main, port=7777)
+    
